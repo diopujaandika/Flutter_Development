3a540a2f96766e21f4a6d33f9784168d866e0984
diopujaandika <dpujaandika17@gmail.com>
2025-07-10 14:37:31 +0700

    first scaffold

---
+++ basic_flutter/lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+///Scaffold
+///Scaffold adalah widget yang digunakan untuk membuat tampilan dasar material design pada aplikasi Flutter
+///Scaffold default terdiri dari;
+///1. AppBar
+///2. Body,
+///3. FloatingActionButton
+
+class FirstScreen extends StatelessWidget{
+  const FirstScreen({Key? key} : super(key: key));
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold();
+  }
+}
