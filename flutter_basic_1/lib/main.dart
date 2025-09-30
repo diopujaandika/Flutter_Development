ce1821fb82bb3bb61312da4532ce9959d8773550
diopujaandika <dpujaandika17@gmail.com>
2025-09-30 22:24:49 +0700

    update

--- flutter_basic_1/lib/main.dart
+++ flutter_basic_1/lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 import 'flutter_basic/3_stateful/stateful1.dart';
 // import 'flutter_basic/2_stateless/stateless1.dart';
-// import 'flutter_basic/1_learn_widget/widget1.dart';
+// import 'flutter_basic/1_widget/widget1.dart';
 // import 'package:flutter_basic_1/flutter_basic/1_widget.dart';
 void main() {
   // runApp(const MyApp());
