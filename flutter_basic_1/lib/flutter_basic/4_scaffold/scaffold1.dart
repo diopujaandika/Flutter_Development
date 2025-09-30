69e804d25095c34ca4ddcde38bbe71d76434cb2a
diopujaandika <dpujaandika17@gmail.com>
2025-09-30 22:33:59 +0700

    scaffold update

--- flutter_basic_1/lib/flutter_basic/4_scaffold/scaffold1.dart
+++ flutter_basic_1/lib/flutter_basic/4_scaffold/scaffold1.dart
@@ -1,5 +1,19 @@
 import 'package:flutter/material.dart';
 
+class MyApp extends StatelessWidget{
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context){
+    return MaterialApp(
+      title: "Flutter Demo",
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const FirstScreen()
+    );
+  }
+}
 class FirstScreen extends StatelessWidget{
   const FirstScreen({Key? key}) : super(key: key);
 
--- flutter_basic_1/lib/main.dart
+++ flutter_basic_1/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
-import 'flutter_basic/3_stateful/stateful1.dart';
+import 'flutter_basic/4_scaffold/scaffold1.dart';
+// import 'flutter_basic/3_stateful/stateful1.dart';
 // import 'flutter_basic/2_stateless/stateless1.dart';
 // import 'flutter_basic/1_widget/widget1.dart';
 // import 'package:flutter_basic_1/flutter_basic/1_widget.dart';
