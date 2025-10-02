25c8fb24b38fbee727f49be9a02cb0ef46df4166
diopujaandika <dpujaandika17@gmail.com>
2025-10-02 15:21:42 +0700

    make a elevated button in flutter basic

---
+++ flutter_basic_1/lib/flutter_basic/7_button/button1.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+
+class MyApp extends StatelessWidget{
+  const MyApp ({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context){
+    return MaterialApp(
+      title: 'Flutter App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget{
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      appBar: AppBar(),
+      body: SafeArea(
+          child: ListView(
+            children: <Widget>[
+              Column(
+                children: <Widget>[
+                  ElevatedButton(
+                      onPressed: (){},
+                      child: const Text(
+                        "Tombol",
+                      ),
+                  ),
+                ],
+              )
+            ],
+          )
+      ),
+      floatingActionButton: IconButton(
+          onPressed: (){},
+          icon: Icon(
+            Icons.plus_one,
+          ),
+      ) ,
+    );
+  }
+}
