530f364fcc8a9ab3647ba4a77cb3a1087444b821
diopujaandika <dpujaandika17@gmail.com>
2025-10-03 08:19:57 +0700

    update

--- flutter_basic_1/lib/flutter_basic/8_input/checkbox1.dart
+++ flutter_basic_1/lib/flutter_basic/8_input/checkbox1.dart
@@ -1,1 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_basic_1/flutter_basic/4_scaffold/scaffold1.dart';
+
+class MyApp extends StatelessWidget{
+  const MyApp ({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context){
+    return MaterialApp(
+      title: 'Flutter Demo',
+      home: FirstScreen(),
+    );
+  }
+}
