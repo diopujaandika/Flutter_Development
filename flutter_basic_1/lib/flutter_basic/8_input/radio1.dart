2285a92c706d3bdc83725ee17bd5152c928c9fe2
diopujaandika <dpujaandika17@gmail.com>
2025-10-03 08:15:47 +0700

    make a radio input in flutter basic

--- flutter_basic_1/lib/flutter_basic/8_input/radio1.dart
+++ flutter_basic_1/lib/flutter_basic/8_input/radio1.dart
@@ -47,12 +47,47 @@ class _FirstScreenState  extends State<FirstScreen>{
               onChanged: (String? value){
                 setState((){
                   language = value;
-                  showSnavkbar();
+                  showSnackbar();
                 });
               },
-            )
+            ),
+            title: Text('Dart'),
+          ),
+          ListTile(
+            leading: Radio<String>(
+              value: 'Kotlin',
+              groupValue: language,
+              onChanged: (String? value){
+                setState(() {
+                  language = value;
+                  showSnackbar();
+                });
+              },
+            ),
+            title: Text('Kotlin'),
+          ),
+          ListTile(
+            leading: Radio<String>(
+              value: "Swift",
+              groupValue: language,
+              onChanged: (String? value){
+                setState(() {
+                  language = value;
+                  showSnackbar();
+                });
+              },
+            ),
+            title: Text('Swift'),
           )
         ],
+      )
+    );
+  }
+  void showSnackbar(){
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        content: Text('$language selected'),
+        duration: Duration(seconds: 1),
       )
     );
   }
